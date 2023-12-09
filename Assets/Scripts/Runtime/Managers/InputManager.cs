dfb7abfdeda043e82a90847f859ad5d9a37af185
atakandll <atakanccnar@outlook.com>
2023-12-09 14:58:47 +0300

    Input system (added joystick logic)

---
+++ Assets/Scripts/Runtime/Managers/InputManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Runtime.Signals;
+using UnityEngine;
+
+namespace Runtime.Managers
+{
+    public class InputManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serialized Variables
+
+        [SerializeField] private FloatingJoystick floatingJoystick;
+        
+        #endregion
+
+        #region Private Variables
+        
+        private bool _isFirstTouchTaken;
+        private bool _hasTouch;
+        private bool _isReleased;
+        private bool _isClick;
+        
+        #endregion
+
+        #endregion
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+           CoreGameSignals.Instance.onPlay += OnPlay;
+           CoreGameSignals.Instance.onReset += OnReset;
+        }
+
+        private void UnsubscribeEvents()
+        {
+            CoreGameSignals.Instance.onPlay -= OnPlay;
+            CoreGameSignals.Instance.onReset -= OnReset;
+            
+        }
+
+        private void OnDisable()
+        {
+            UnsubscribeEvents();
+        }
+
+        private void Update()
+        {
+            if (true)
+            {
+                float directionAngel = Mathf.Rad2Deg * Mathf.Atan2(floatingJoystick.Horizontal, floatingJoystick.Vertical);
+
+                Vector3 joystickDirection = new Vector3(floatingJoystick.Horizontal,0, floatingJoystick.Vertical);
+
+                if (joystickDirection.magnitude>0)
+                {
+                    _isClick = false;
+                }
+
+                if (Input.GetMouseButton(0) && !_isClick)
+                {
+                    _hasTouch = true;
+                }
+                else
+                {
+                    _hasTouch = false;
+                }
+
+                if (_hasTouch)
+                {
+                    InputSignals.Instance.onInputTouch?.Invoke(directionAngel, joystickDirection);
+                }
+                else
+                {
+                    InputSignals.Instance.onInputReleased?.Invoke();
+                }
+            }
+        }
+
+        private void OnPlay() => StartToInput();
+
+        private void OnReset() => StopToInput();
+
+        private void StartToInput() => _isFirstTouchTaken = true;
+        private void StopToInput() => _isFirstTouchTaken = false;
+
+
+    }
+}
---
+++ Assets/Scripts/Runtime/Signals/InputSignals.cs
@@ -0,0 +1,12 @@
+﻿using Runtime.Extensions;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Runtime.Signals
+{
+    public class InputSignals : MonoSingleton<InputSignals>
+    {
+        public UnityAction<float, Vector3> onInputTouch = delegate { };
+        public UnityAction onInputReleased = delegate { };
+    }
+}
