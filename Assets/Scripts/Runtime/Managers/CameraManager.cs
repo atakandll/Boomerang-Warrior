415100e9817826cadb872a38d01a678ea7a8f326
atakandll <atakanccnar@outlook.com>
2023-12-09 20:14:14 +0300

    Camera logic and Cinemachine added

---
+++ Assets/Scripts/Runtime/Managers/CameraManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Cinemachine;
+using Runtime.Signals;
+using UnityEngine;
+
+namespace Runtime.Managers
+{
+    public class CameraManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serialized Variables
+
+        [SerializeField] private CinemachineVirtualCamera virtualCamera;
+
+        #endregion
+
+        #endregion
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            CameraSignals.Instance.onSpawnPlayer += OnSpawnPlayer;
+        }
+
+        private void UnsubscribeEvents()
+        {
+            CameraSignals.Instance.onSpawnPlayer -= OnSpawnPlayer;
+            
+        }
+
+        private void OnDisable()
+        {
+            UnsubscribeEvents();
+        }
+
+        private float OnSpawnPlayer(GameObject player)
+        {
+            virtualCamera.Follow = player.transform;
+            return virtualCamera.transform.rotation.eulerAngles.y;
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Signals/CameraSignals.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Runtime.Extensions;
+using UnityEngine;
+
+namespace Runtime.Signals
+{
+    public class CameraSignals : MonoSingleton<CameraSignals>
+    {
+        public Func<GameObject,float> onSpawnPlayer = delegate { return 0; };
+    }
+}
